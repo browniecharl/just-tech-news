ce90975e677fc5eef12a90e9bee2bd3bc8ed1c26
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-05-31 16:39:44 -0400

    finished module

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { resourceLimits } = require('worker_threads');
-const { User } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -19,7 +18,27 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -54,19 +73,20 @@ router.post('/login', (req, res) => {
     where: {
       email: req.body.email
     }
-  }).then (dbUserData => {
+  }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!'});
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-    // res.json({ user: dbUserData });
-    //verify user
+
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-    res.json({ user: dbUserData, message: 'You are now logged in!'});
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
